d01543dd7895c401352b42885665e9108c823e3e
uditbairi <138181695+uditnarayan1234@users.noreply.github.com>
2026-01-04 19:56:10 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import numpy as np
-import cv2
 import tensorflow as tf
 import faiss
 from sklearn.preprocessing import normalize
 from PIL import Image
+from PIL import Image
+import numpy as np
 
 st.set_page_config(page_title="Image Similarity Search", layout="wide")
 
@@ -23,16 +24,19 @@ interpreter, index, image_paths = load_all()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
+
 def preprocess(img):
-    img = cv2.resize(img, (224, 224))
-    return img.astype("uint8")  # 🔥 INT8 expects uint8
+    img = Image.fromarray(img)
+    img = img.resize((224, 224))
+    img = np.array(img) / 255.0
+    return img.astype("float32")
 
 def get_embedding(img):
     img = preprocess(img)[None, ...]
     interpreter.set_tensor(input_details[0]["index"], img)
     interpreter.invoke()
-    emb = interpreter.get_tensor(output_details[0]["index"]).astype("float32")
-    return normalize(emb)
+    emb = interpreter.get_tensor(output_details[0]["index"])
+    return normalize(emb).astype("float32")
 
 st.title("🔍 AI-Powered Image Similarity Search")
 
@@ -52,4 +56,3 @@ if uploaded:
     for col, i in zip(cols, idx[0]):
         sim_img = Image.open(image_paths[i])
         col.image(sim_img, use_column_width=True)
-
